3f85fdc7b4e00e36ac1e86c28ec0f715dbff96e9
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-11-03 18:18:13 +0200

    fixed: start issues in model User

--- tracker/models.py
+++ tracker/models.py
@@ -1,5 +1,21 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
 
 
 class User(AbstractUser):
     """Custom user model."""
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_groups",  # Unique related name
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_permissions",  # Unique related name
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
