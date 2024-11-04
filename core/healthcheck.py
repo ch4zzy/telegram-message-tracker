214343631bf041c54820fbab4b30070a1e2fef6a
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-11-04 19:44:46 +0200

    fix healthcheck

--- core/healthcheck.py
+++
@@ -1,5 +0,0 @@
-from django.http import JsonResponse
-
-
-def health_check(request: object) -> JsonResponse:
-    return JsonResponse({"status": "ok"})
--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from healthcheck import health_check
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("healthcheck/", health_check),
 ]
