37a4092576fc5cf2c0355bee4c2d091e0abae037
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-11-23 15:49:16 +0200

    add admin interface for User, Post, SourceChannel, and TargetChannel models

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,1 +1,30 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import Post, SourceChannel, TargetChannel, User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
+
+
+@admin.register(TargetChannel)
+class TargetChannelAdmin(admin.ModelAdmin):
+    list_display = ("name", "source_link", "created_at", "auto_post")
+    search_fields = ("name", "source_link")
+    list_filter = ("auto_post",)
+
+
+@admin.register(SourceChannel)
+class SourceChannelAdmin(admin.ModelAdmin):
+    list_display = ("name", "source_link", "created_at", "active_following")
+    search_fields = ("name", "source_link")
+    list_filter = ("active_following",)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("channel", "created_at", "edited_at", "status")
+    search_fields = ("title", "source_channel")
+    list_filter = ("status",)
