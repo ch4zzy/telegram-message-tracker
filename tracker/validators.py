b125ecc8a1486383d9b7a1d8ad3be9437b823255
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-12-05 23:42:53 +0200

    feat: add URL validation and status check for Telegram channels

--- tracker/validators.py
+++ tracker/validators.py
@@ -1,5 +1,8 @@
+import re
 from urllib.parse import urlparse
 
+import requests
+from django.conf import settings
 from django.core.exceptions import ValidationError
 
 
@@ -8,3 +11,21 @@ def validate_url(value: str) -> None:
     parsed_url = urlparse(value)
     if parsed_url.netloc not in ["t.me", "www.t.me"]:
         raise ValidationError("URL is not from Telegram.")
+
+
+def validate_status_200(value: str) -> None:
+    """Validate URL and check if it returns 200."""
+    token = settings.BOT_TOKEN
+    match = re.search(r"t\.me/([a-zA-Z0-9_]+)", value)
+    if match:
+        channel_username = match.group(1)
+    else:
+        raise ValidationError("Invalid URL.")
+
+    url = f"https://api.telegram.org/bot{token}/getChat"
+    params = {"chat_id": f"@{channel_username}"}
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        pass
+    else:
+        raise ValidationError("Channel does not exist.")
