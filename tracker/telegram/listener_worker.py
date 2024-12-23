ab0cc425202d408c7035511ecd6c7e3729639348
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-12-23 14:55:40 +0200

    refactor: improve code readability and organization in listener_worker.py

--- tracker/telegram/listener_worker.py
+++ tracker/telegram/listener_worker.py
@@ -1,6 +1,7 @@
-from django.conf import settings
 from asgiref.sync import sync_to_async
+from django.conf import settings
 from telethon.sync import TelegramClient
+
 from tracker.telegram.telethon_client import TelethonClient
 
 
@@ -31,7 +32,7 @@ class LastMessageTracker:
 telethon_client = TelethonClient(
     api_id=settings.API_ID,
     api_hash=settings.API_HASH,
-    session=settings.TELEGRAM_SESSION
+    session=settings.TELEGRAM_SESSION,
 )
 
 telethon_client.set_client()
@@ -42,17 +43,18 @@ message_tracker = LastMessageTracker()
 @sync_to_async
 def get_active_channels():
     from tracker.models import SourceChannel
+
     return list(
-        SourceChannel.objects.all().filter(
-            active_following=True, 
-            verified_status=True
-        ).values_list("source_link", flat=True)
+        SourceChannel.objects.all()
+        .filter(active_following=True, verified_status=True)
+        .values_list("source_link", flat=True)
     )
 
 
 @sync_to_async
 def save_post(channel, message):
-    from tracker.models import SourceChannel, Post
+    from tracker.models import Post, SourceChannel
+
     post = Post.objects.create(
         channel=SourceChannel.objects.get(source_link=channel),
         content=message.message,
@@ -60,12 +62,12 @@ def save_post(channel, message):
     post.save()
 
 
-async def fetch_new_posts(client: TelegramClient):    
+async def fetch_new_posts(client: TelegramClient):
     channels = await get_active_channels()
 
     if not message_tracker._last_message_time:
         message_tracker.initialize(channels)
-    
+
     message_tracker.update(channels)
 
     for channel in channels:
@@ -74,7 +76,11 @@ async def fetch_new_posts(client: TelegramClient):
             async for message in client.iter_messages(channel, limit=1):
                 message_tracker.set_last_message_time(channel, message.date)
         else:
-            async for message in client.iter_messages(channel, offset_date=last_time, reverse=True):
+            async for message in client.iter_messages(
+                channel, offset_date=last_time, reverse=True
+            ):
                 if message.date > last_time:
                     await save_post(channel, message)
-                    message_tracker.set_last_message_time(channel, message.date)
+                    message_tracker.set_last_message_time(
+                        channel, message.date
+                    )
