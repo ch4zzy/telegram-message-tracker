b7c040f14c3eba15016d68db322ffd4c3fcc1d08
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-12-23 14:52:09 +0200

    feat: add post message signal for pending posts and streamline task imports

--- tracker/signals.py
+++ tracker/signals.py
@@ -1,8 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import SourceChannel, TargetChannel
-from .tasks import validate_admin_bot_task, validate_link_task  # noqa
+from .models import Post, SourceChannel, TargetChannel
+from .tasks import (  # noqa
+    post_message_task,
+    validate_admin_bot_task,
+    validate_link_task,
+)
 
 
 @receiver(post_save, sender=SourceChannel)
@@ -16,3 +20,13 @@ def channel_verification_signal(sender, instance, created, **kwargs):
 def admin_verification_signal(sender, instance, created, **kwargs):
     if not created and instance.admin_status is False:
         validate_admin_bot_task.delay(instance.id)
+
+
+@receiver(post_save, sender=Post)
+def post_message_signal(sender, instance, created, **kwargs):
+    if created and instance.status == "pending":
+        target_channels = instance.channel.target_channel.filter(
+            auto_post=True, verified_status=True, admin_status=True
+        )
+        if target_channels.exists():
+            post_message_task.delay(instance.id)
