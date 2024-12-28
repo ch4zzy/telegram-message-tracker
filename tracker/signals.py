0007dec33f1ac65a1e26f0d7279dbefd9eee775c
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2024-12-28 18:37:46 +0200

    feat: enhance admin verification signal to include verified status check

--- tracker/signals.py
+++ tracker/signals.py
@@ -14,5 +14,9 @@ def channel_verification_signal(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=TargetChannel)
 def admin_verification_signal(sender, instance, created, **kwargs):
-    if not created and instance.admin_status is False:
+    if (
+        not created
+        and instance.admin_status is False
+        and instance.verified_status is True
+    ):
         validate_admin_bot_task.delay(instance.id)
