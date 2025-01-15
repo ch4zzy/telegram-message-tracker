12770384cff7486dc2fea92a2be6e4f933021148
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-01-15 16:30:41 +0200

    add signup form

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import SourceChannel, TargetChannel
 from .validators import validate_source_target_unique
@@ -26,3 +28,24 @@ class SourceChannelForm(forms.ModelForm):
             if error_message:
                 self.add_error("target_channel", error_message)
         return cleaned_data
+
+
+class SignUpForm(UserCreationForm):
+    username = forms.CharField(max_length=30, required=True, label="Username")
+    email = forms.EmailField(
+        max_length=254, required=True, help_text="Enter a valid email address."
+    )
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Password")
+    password2 = forms.CharField(
+        widget=forms.PasswordInput, label="Confirm Password"
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "email", "password1", "password2"]
+
+    def match_password(self):
+        cd = self.cleaned_data
+        if cd["password1"] != cd["password2"]:
+            raise forms.ValidationError("Passwords dont match.")
+        return cd["password2"]
