8efea1d416f1733c04605ce2995199a87e9c63ca
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-01-15 16:33:22 +0200

    add login signup urls

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
+
+from tracker import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("tracker.urls")),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("signup/", views.signup, name="signup"),
 ]
