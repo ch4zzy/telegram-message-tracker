ec5a3611168939be731d3c3f9a44e48f4fb7e6a6
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-02-01 21:11:46 +0200

    add source management URLs and filters for create, update, delete, and search functionalities

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,12 +3,34 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("slist/", views.source_channel_list, name="source_channel_list"),
+    path("source/", views.source_list, name="source_list_partial"),
 ]
 
 htmx_patterns = [
     path("check-username/", views.check_username, name="check_username"),
     path("check-email/", views.check_email, name="check_email"),
+    # Edit
+    path("create-source/", views.create_source, name="create_source"),
+    path(
+        "update-source-status/<int:pk>",
+        views.update_source_status,
+        name="update_source_status",
+    ),
+    path("update-source/<int:pk>", views.update_source, name="update_source"),
+    path("delete-source/<int:pk>", views.delete_source, name="delete_source"),
+    # Search
+    path("search-source/", views.search_source, name="source_search"),
+    # Statuses
+    path(
+        "source-validate/<int:pk>/",
+        views.check_source_link,
+        name="check_source_link",
+    ),
+    path(
+        "check-task-status/<int:pk>/",
+        views.check_task_status,
+        name="check_task_status",
+    ),
 ]
 
 urlpatterns += htmx_patterns
