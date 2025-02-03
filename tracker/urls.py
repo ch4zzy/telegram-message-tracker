93f020de2f8dd26d0130d7390e799a8a6f227277
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2025-02-03 19:05:46 +0200

    add url for new target views

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("source/", views.source_list, name="source_list_partial"),
+    path("target/", views.target_list, name="target_list_partial"),
 ]
 
 htmx_patterns = [
@@ -11,15 +12,24 @@ htmx_patterns = [
     path("check-email/", views.check_email, name="check_email"),
     # Edit
     path("create-source/", views.create_source, name="create_source"),
+    path("create-target/", views.create_target, name="create_target"),
     path(
         "update-source-status/<int:pk>",
         views.update_source_status,
         name="update_source_status",
     ),
+    path(
+        "update-target-status/<int:pk>",
+        views.update_target_status,
+        name="update_target_status",
+    ),
     path("update-source/<int:pk>", views.update_source, name="update_source"),
+    path("update-target/<int:pk>", views.update_target, name="update_target"),
     path("delete-source/<int:pk>", views.delete_source, name="delete_source"),
+    path("delete-target/<int:pk>", views.delete_target, name="delete_target"),
     # Search
     path("search-source/", views.search_source, name="source_search"),
+    path("search-target/", views.search_target, name="target_search"),
     # Statuses
     path(
         "source-validate/<int:pk>/",
@@ -27,10 +37,17 @@ htmx_patterns = [
         name="check_source_link",
     ),
     path(
-        "check-task-status/<int:pk>/",
-        views.check_task_status,
-        name="check_task_status",
+        "target-validate/<int:pk>/",
+        views.check_target_link,
+        name="check_target_link",
     ),
+    path(
+        "target-bot-validate/<int:pk>/",
+        views.check_target_bot,
+        name="check_target_bot",
+    ),
+    path("source/<int:pk>/", views.get_source, name="get_source"),
+    path("target/<int:pk>/", views.get_target, name="get_target"),
 ]
 
 urlpatterns += htmx_patterns
